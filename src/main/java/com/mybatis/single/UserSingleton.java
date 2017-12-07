5ca1a4925edd3640d37fdbf04b1313b47baafbc0
JiongJiongGe <wangjiongda@163.com>
2017-12-07 10:11:40 +0800

    单例private

--- src/main/java/com/mybatis/single/UserSingleton.java
+++ src/main/java/com/mybatis/single/UserSingleton.java
@@ -12,15 +12,30 @@ import java.util.LinkedHashMap;
 /**
  * Created by yunkai on 2017/10/11.
  */
+@Component
 public class UserSingleton {
 
     private static Logger logger = LoggerFactory.getLogger(UserSingleton.class);
 
     private LinkedHashMap<String, String> linkMap;
 
-    public volatile static UserSingleton userSingleton;
+    private volatile static UserSingleton userSingleton;
 
     private UserSingleton(){}
+
+    public static LinkedHashMap<String, String> getUserSingleton(){
+        if(userSingleton == null){
+            synchronized (UserSingleton.class){
+                if(userSingleton == null){
+                    userSingleton = new UserSingleton();
+                    LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+                    userSingleton.setLinkMap(map);
+                }
+            }
+        }
+        logger.info("userSingleton = {}", new Gson().toJson(userSingleton));
+        return userSingleton.getLinkMap();
+    }
 
     public static String getUserSingletonValue(String key){
         if(userSingleton == null){
@@ -45,6 +60,14 @@ public class UserSingleton {
             }
         }
         logger.info("userSingleton = {}", new Gson().toJson(userSingleton));
+        return userSingleton;
+    }
+
+    public static UserSingleton setUserSingletonMap(LinkedHashMap<String, String> map){
+        if(userSingleton == null){
+            userSingleton = new UserSingleton();
+        }
+        userSingleton.setLinkMap(map);
         return userSingleton;
     }
 
